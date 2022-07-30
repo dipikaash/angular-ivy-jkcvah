b18e2f582aad457173964a386e39eb699f91697f
dipikaash <ash.dipika2012@gmail.com>
2022-07-30 13:52:20 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component, VERSION } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Angular ' + VERSION.major;
+  detail = [];
+  fg: FormGroup;
+  constructor(public fb: FormBuilder) {}
+  onTDSubmit(f: NgForm) {
+    console.log(f.value);
+    this.detail.push({ name: f.value.name, age: f.value.age });
+  }
+  ngOnInit() {
+    this.fg = this.fb.group({
+      name: new FormControl(null),
+      age: new FormControl(null),
+    });
+  }
+  onReactiveSubmit() {
+    console.log(this.fg.value);
+    this.detail.push(
+      this.fg.value
+      // {name: this.fg.value.name,
+      // age: this.fg.value.age,}
+    );
+  }
+}
